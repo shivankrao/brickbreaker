7e013927691fba889836179ac02e2a5181bb4ec6
Shivank Rao <shivankrao@Shivanks-MacBook-Pro.local>
2018-10-15 14:02:55 -0400

    added readme

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -82,8 +82,8 @@ bg.scale.setTo(80, 60);
  }
  
 
-//     //BALL
-//         //adding ball sprite to game
+     //BALL
+         //adding ball sprite to game
         ball = game.add.sprite(game.world.width*0.5, game.world.height-110, 'ball');
         ball.anchor.set(0.5);        
         //enabling arcade physics onto ball
@@ -122,7 +122,7 @@ bg.scale.setTo(80, 60);
     
             
 
-//     //PADDLE
+        //PADDLE
             //adding paddle sprite to game
             paddle = game.add.sprite(game.world.width*0.5, game.world.height-45, 'paddle');
             //sets anchor on paddle from bottom edge
@@ -235,9 +235,11 @@ function ballLeaveScreen() {
     if(lives) {
         livesText.setText('Lives: '+lives);
         lifeLost.visible = true;
+
         //resetting assets
         ball.reset(game.world.width*0.5, game.world.height-25);
         paddle.reset(game.world.width*0.5, game.world.height-5);
+
         game.input.onDown.addOnce(function(){
             lifeLost.visible = false;
             ball.body.velocity.set(150, -150);
@@ -273,6 +275,7 @@ function ballHitPaddle(ball, paddle) {
 
 // };
 
+
 // //start game
 
 // //game initializes and player clicks start to begin playing
@@ -292,10 +295,10 @@ function ballHitPaddle(ball, paddle) {
 
 
 
-//     ////at the beginning of the game, the paddle starts off big
-//     ////the ball moves relatively slow
-//     ////as the player advances through levels, the paddle becomes smaller, and the ball moves faster
-//     ////I want the number of bricks in each level to remain the same (space constraints), but the positions of the bricks to be randomized with each level. 
+     ////at the beginning of the game, the paddle starts off big
+     ////the ball moves relatively slow
+     ////as the player advances through levels, the paddle becomes smaller, and the ball moves faster
+     ////I want the number of bricks in each level to remain the same (space constraints), but the positions of the bricks to be randomized with each level. 
 
 // //the ball bounces off the bricks and when it hits the brick, the brick disappears
 
@@ -303,17 +306,17 @@ function ballHitPaddle(ball, paddle) {
 
 
 
-//     ////I want an animation when the brick is destroyed
+     ////I want an animation when the brick is destroyed
 
-// //every time the ball hits a brick, the player gains one point
-
-
+ //every time the ball hits a brick, the player gains one point
 
 
 
-//     //I want it so that in each level, a few bricks will randomly give the user extra points (color coded by amount of points)
 
-// //each player is given 5 lives
-// //if the ball does not land on the brick, the player loses a life 
 
-//     //some bricks will randomly grant the user with an extra life
+//I want it so that in each level, a few bricks will randomly give the user extra points (color coded by amount of points)
+
+    //each player is given 5 lives
+    //if the ball does not land on the brick, the player loses a life 
+
+     //some bricks will randomly grant the user with an extra life
